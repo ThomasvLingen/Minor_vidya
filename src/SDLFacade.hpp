18b5e8f32f987e4ec2d669c9b370fda6ee263bee
Wouter <wgeel@avans.nl>
2016-11-02 13:07:34 +0100

    readded the removed .hpp changes

--- src/SDLFacade.hpp
+++ src/SDLFacade.hpp
@@ -11,8 +11,8 @@
 
 #include "Color.hpp"
 
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_ttf.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 using std::string;
 using std::map;
@@ -31,11 +31,22 @@ class SDLFacade {
 
         map<FontType, TTF_Font *> _fonts;
         map<SDL_Keycode, Key> _possible_keys;
+        SDL_Window* window = NULL;
+        SDL_Surface* screenSurface = NULL;
+        SDL_Renderer* renderer = NULL;
 
     public:
         SDLFacade();
 
         virtual ~SDLFacade();
+
+        bool init();
+
+        void clear_screen(); //clears the surface
+
+        void draw_line(const double &x1, const double &y1, const double &x2, const double &y2, const Color &color);
+
+        void render_buffer() const;
 
         void handle_sdl_events();
 
@@ -43,10 +54,19 @@ class SDLFacade {
 
         void draw_text(string text, FontType font) const;
 
+        void set_height(const int &screen_height, const int &screen_width);
+
+        int get_height() const;
+
+        void set_width(const int &screen_height, const int &screen_width);
+
+        int get_width() const;
 
     private:
         void _handle_quit_event();
         void _handle_key_event();
+        bool init_window();
+        bool init_renderer();
 
 };
 
