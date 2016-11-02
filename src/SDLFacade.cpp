a99d08c496ddf49818952dcc602006c7935cb364
Wouter <wgeel@avans.nl>
2016-11-02 13:27:13 +0100

    loaded from other branch (WouterMartijn_SDLFacade)

--- src/SDLFacade.cpp
+++ src/SDLFacade.cpp
@@ -12,6 +12,77 @@ SDLFacade::~SDLFacade() {
     //todo
 }
 
+bool SDLFacade::init() {
+
+    // Init SDL
+    if(SDL_Init(SDL_INIT_EVERYTHING < 0)) {
+        printf("Something went wrong while initting: %s\n", SDL_GetError());
+        return false;
+    }
+
+    // Init SDL_TTF
+    if (TTF_Init() < 0) {
+        printf("Something went wrong while initting SDL_TTF!%s\n", TTF_GetError());
+    };
+
+    // Init window
+    printf("initting window\n");
+    if (!this->init_window()) {
+        return false;
+    }
+
+    printf("initting renderer\n");
+    if (!this->init_renderer()) {
+        return false;
+    }
+
+    printf("initting done\n");
+    return true;
+}
+
+bool SDLFacade::init_renderer()
+{
+    // Attempt to make a software renderer
+    this->renderer = SDL_CreateRenderer(this->window, -1, SDL_RENDERER_SOFTWARE);
+    if (this->renderer != NULL) {
+        printf("Warning: could not use hardware acceleration --> SDL_Renderer uses software rendering\n");
+        return true;
+    } else {
+        printf("Something went wrong while making a renderer! : %s\n", SDL_GetError());
+        return false;
+    }
+}
+
+bool SDLFacade::init_window() {
+    window = SDL_CreateWindow("Vidya gaem", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500,
+                              500, SDL_WINDOW_SHOWN);
+    if(window != NULL) {
+        screenSurface = SDL_GetWindowSurface(window);
+        return true;
+    } else {
+        printf("Something went wrong while making a window! : %s\n", SDL_GetError());
+        return false;
+    }
+}
+
+void SDLFacade::clear_screen() {
+    // Draw background
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+}
+
+void SDLFacade::draw_line(const double &x1, const double &y1, const double &x2, const double &y2, const Color &color) {
+    //todo
+    SDL_RenderDrawLine(renderer, 0, 0, 20, 30 );
+    //SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+
+}
+
+void SDLFacade::render_buffer() const {
+    //todo
+    // Update window surface
+    SDL_UpdateWindowSurface(window);
+}
+
 
 void SDLFacade::handle_sdl_events() {
     //todo
@@ -20,6 +91,28 @@ void SDLFacade::handle_sdl_events() {
 std::vector <Key, std::allocator<Key>> SDLFacade::get_keys() const {
     //todo
     return std::vector < Key, std::allocator < Key >> ();
+}
+
+void SDLFacade::draw_text(string text, FontType font) const {
+    //todo
+}
+
+void SDLFacade::set_height(const int &screen_height, const int &screen_width) {
+    //todo
+}
+
+int SDLFacade::get_height() const {
+    //todo
+    return 0;
+}
+
+void SDLFacade::set_width(const int &screen_height, const int &screen_width) {
+    //todo
+}
+
+int SDLFacade::get_width() const {
+    //todo
+    return 0;
 }
 
 void SDLFacade::_handle_quit_event() {
--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,19 @@ int main()
 {
     std::cout << "Hello world" << std::endl;
 
-//    SDLFacade sdlFacade = SDLFacade();
-//    if(sdlFacade.init()){
-//        std::cout << "Start" << std::endl;
-//        while(true) {
-//            sdlFacade.clear_screen();
-//            sdlFacade.draw_line(0, 0, 20, 20, Color());
-//            sdlFacade.render_buffer();
-//            SDL_Delay(1000);
-//        }
-//    } else {
-//        std::cout << "Failed" << std::endl;
-//    }
-//    std::cin;
+    SDLFacade sdlFacade = SDLFacade();
+    if(sdlFacade.init()){
+        std::cout << "Start" << std::endl;
+        while(true) {
+            sdlFacade.clear_screen();
+            sdlFacade.draw_line(0, 0, 20, 20, Color());
+            sdlFacade.render_buffer();
+            SDL_Delay(1000);
+        }
+    } else {
+        std::cout << "Failed" << std::endl;
+    }
+    std::cin;
 
     return 0;
 }
