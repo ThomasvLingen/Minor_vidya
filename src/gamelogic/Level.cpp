fb3e2fc72881aa7ea881d4ad0bab2e6c53aed6e5
Jelmer <jschoot2@student.avans.nl>
2016-11-07 14:54:00 +0100

    feat: Added basic gamelogic classes with some logic

--- src/gamelogic/Level.cpp
+++ src/gamelogic/Level.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Level.hpp"
+#include "Tile.hpp"
+
+using std::vector
+
+Level::Level(vector<vector<TileObject*>> field)
+: WorldObject(field)
+, _point_of_view(Engine::DoubleCoordinate{2 , 10}, Engine::RaycastingVector{-1 , 0}, Engine::RaycastingVector{0 , 0.66})
+{
+
+}
+
+Level::~Level(){
+
+}
+
+void Level::update()
+{
+    // TODO: Implement this
+}
+
+PointOfView & Level::get_pov()
+{
+    return this->_point_of_view;
+}
+
+TileObject * Level::get_tile(int x, int y)
+{
+    try {
+        return this->_field[y][x];
+    }
+    catch (const std::exception&) {
+        return nullptr;
+    }
+}
+
+DoubleCoordinate Level::get_spawnpoint()
+{
+    return this->_spawnpoint;
+}
--- src/gamelogic/Player.cpp
+++ src/gamelogic/Player.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Player.hpp"
+
+Player::Player(DoubleCoordinate position)
+: PointOfView(position, Engine::RaycastingVector{-1 , 0}, Engine::RaycastingVector{0 , 0.66})
+{
+
+}
+
+Player::~Player()
+{
+
+}
+
+DoubleCoordinate &Engine::PointOfView::get_position()
+{
+    return this->_position;
+}
+
+RaycastingVector &Engine::PointOfView::get_direction()
+{
+    return this->_direction;
+}
+
+RaycastingVector &Engine::PointOfView::get_camera_plane()
+{
+    return this->_camera_plane;
+}
