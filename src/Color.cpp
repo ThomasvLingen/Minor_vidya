91f691936b0d9849166966b07bfc80baacc170b6
Wouter <wgeel@avans.nl>
2016-11-11 17:21:54 +0100

    feat: color reduce function implemented

--- src/Color.cpp
+++ src/Color.cpp
@@ -2,13 +2,21 @@
 // Created by wouter on 11/7/16.
 //
 #include "Color.hpp"
+namespace Engine {
+    Color Color::reduce_intensity() {
+        uint8_t temp_r_value = this->r_value / 2;
+        uint8_t temp_g_value = this->g_value / 2;
+        uint8_t temp_b_value = this->b_value / 2;
 
-Color Color::reduce_intensity() {
-    this->r_value = this->r_value / 2;
-    this->g_value = this->g_value / 2;
-    this->b_value = this->b_value / 2;
-    return this;
-};
+        Color return_color = {
+                .r_value = temp_r_value,
+                .g_value = temp_g_value,
+                .b_value = temp_b_value
+        };
+
+        return return_color;
+    };
+}
 
 
 
