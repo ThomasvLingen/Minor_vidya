bf722123b8080c51009d6bf15540b50571563ba6
Wouter <wgeel@avans.nl>
2016-11-11 16:37:34 +0100

    fix: & was placed in a situation where this was inappropriate

--- src/Color.cpp
+++ src/Color.cpp
@@ -7,7 +7,7 @@ Color Color::reduce_intensity() {
     this->r_value = this->r_value / 2;
     this->g_value = this->g_value / 2;
     this->b_value = this->b_value / 2;
-    return this&;
+    return this;
 };
 
 
