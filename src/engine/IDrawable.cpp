66cf8e5dcd3422b88957782979f2252c1e963b10
jorgdebont <jacbont@student.avans.nl>
2016-11-18 15:37:55 +0100

    feat: added Idrawable

---
+++ src/engine/IDrawable.cpp
@@ -0,0 +1,17 @@
+//
+// Created by sander on 11/17/16.
+//
+
+#include "IDrawable.hpp"
+
+namespace Engine {
+    IDrawable::IDrawable()
+    {
+
+    }
+
+    IDrawable::~IDrawable()
+    {
+
+    }
+}
---
+++ src/engine/IDrawable.hpp
@@ -0,0 +1,20 @@
+//
+// Created by sander on 11/17/16.
+//
+
+#ifndef MINOR_VIDYA_IDRAWABLE_HPP
+#define MINOR_VIDYA_IDRAWABLE_HPP
+
+namespace Engine {
+    class IDrawable {
+
+    public:
+        IDrawable();
+
+        virtual ~IDrawable();
+
+        virtual void draw() = 0;     // pure virtual func
+
+    };
+}
+#endif //MINOR_VIDYA_IDRAWABLE_HPP
