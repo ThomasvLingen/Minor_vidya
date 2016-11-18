2feb926e340be8f3a150e0104d7406ae5980f5f0
ThomasvLingen <thovali96@gmail.com>
2016-11-18 20:24:21 +0100

    refactor: resolve final issues from PR #38

--- src/engine/SDLFacade.cpp
+++ src/engine/SDLFacade.cpp
@@ -104,7 +104,7 @@ namespace Engine {
     /// \return This function returns True if the _window was successfully initialized, ohterwise it returns False
     bool SDLFacade::_init_window()
     {
-        this->_window = SDL_CreateWindow("Vidya game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500,
+        this->_window = SDL_CreateWindow(Config::GAME_WINDOW_TITLE.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500,
                                          500, SDL_WINDOW_SHOWN);
         if (this->_window != nullptr) {
             this->_screenSurface = SDL_GetWindowSurface(this->_window);
@@ -125,7 +125,7 @@ namespace Engine {
 
         if (this->_screen_buffer != nullptr) {
             return true;
-        }else{
+        } else {
             SDL_DestroyTexture(this->_screen_buffer);
             return false;
         }
