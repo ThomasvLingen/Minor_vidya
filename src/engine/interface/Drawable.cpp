c9c105574bcd94e80f19fd178a36a80b7a2f53ba
jorgdebont <jacbont@student.avans.nl>
2016-11-18 17:13:27 +0100

    fix: drawable now has an SDL facade

--- src/engine/interface/Drawable.cpp
+++ src/engine/interface/Drawable.cpp
@@ -5,7 +5,8 @@
 #include "Drawable.hpp"
 
 namespace Engine {
-    Drawable::Drawable()
+    Drawable::Drawable(SDLFacade& _SDL_facade)
+    : _SDL_facade(_SDL_facade)
     {
 
     }
--- src/engine/interface/Drawable.hpp
+++ src/engine/interface/Drawable.hpp
@@ -5,14 +5,18 @@
 #ifndef MINOR_VIDYA_IDRAWABLE_HPP
 #define MINOR_VIDYA_IDRAWABLE_HPP
 
+#include "SDLFacade.hpp"
+
 namespace Engine {
     class Drawable {
 
     public:
-        Drawable();
+        Drawable(SDLFacade& _SDL_facade);
 
         virtual void draw() = 0;     // pure virtual func
 
+    private:
+        SDLFacade& _SDL_facade;
     };
 }
 #endif //MINOR_VIDYA_IDRAWABLE_HPP
