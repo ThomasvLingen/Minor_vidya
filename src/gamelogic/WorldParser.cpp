9cb907ef053858a3b9d41b6593c5ed0095633907
joostvanrijsinge <jjrijsin@student.avans.nl>
2016-11-20 22:17:52 +0100

    add: added worldparser and rapidxml adapter. file is runnable as features of the world parser are commented for now

---
+++ src/gamelogic/WorldParser.cpp
@@ -0,0 +1,78 @@
+#include "WorldParser.hpp"
+#include <tuple>
+
+namespace GameLogic {
+
+    using std::tuple;
+    using std::get;
+
+    WorldParser::WorldParser()
+    {
+    }
+
+
+    WorldParser::~WorldParser()
+    {
+    }
+
+    /// \brief Generates a level from the source .tmx file
+    /// 
+    /// This function generates a Level object which has:
+    /// * 2D vector of Tiles
+    /// * TileSet struct
+    /// needs to be inside try catch
+    ///
+    /// \param file_location location of the file to be used
+    /// \return returns a Level pointer
+    Level* WorldParser::generate_level( std::string file_location )
+    {
+        RapidXMLAdapter* rapid_adapter = new RapidXMLAdapter();
+        //TileSet* tile_set = new TileSet();
+        vector<vector<size_t>> int_map;
+        vector<vector<Tile*>> map;
+        vector<tuple<size_t, size_t, char*>> object_list;
+
+        rapid_adapter->setup_document( file_location );
+
+
+        //tile_set->texture_source = rapid_adapter->get_texture_source();
+        //tile_set->image_height = rapid_adapter->get_image_height();
+        //tile_set->image_width = rapid_adapter->get_image_width();
+        //tile_set->tile_size = rapid_adapter->get_tile_size();
+
+        int_map = rapid_adapter->get_map();
+
+        for ( int y = 1; y < int_map.size(); y++ ) {
+            vector<Tile*> map_row;
+            for ( int x = 0; x < int_map[y].size(); x++ ) {
+                Tile* new_tile = new Tile;
+                switch ( int_map[x][y] ) {
+                case 1:
+                    new_tile->set_wall( true );
+                    new_tile->set_color( { 255,0,0 } );
+                    break;
+                    map_row.push_back( new_tile );
+                }
+            }
+            map.push_back( map_row );
+        }
+
+        //object_list = rapid_adapter->get_objects();
+
+        //for ( size_t i = 0; i < object_list.size(); i++ ) {
+        //    if ( std::strcmp( get<2>( object_list[i] ), "PlayerSpawn" ) == 0 ) {
+        //        map[get<0>( object_list[i] )][get<1>( object_list[i] )]->type = SPAWN;
+        //    }
+        //    // Other Objects
+        //    //else if ( std::strcmp(get<2>( object_list[i] ), "Other_Object" ) == 0 ) {
+
+        //    //}
+        //}
+
+        // generated_level needs to be deleted in the mainclass/gameloop when this level has been completed/finished/player quits.
+        Level* generated_level = new Level( map );
+        //generated_level->tile_set = tile_set;
+
+        return generated_level;
+    }
+}
---
+++ src/gamelogic/WorldParser.hpp
@@ -0,0 +1,26 @@
+#ifndef WorldParser_HPP
+#define WorldParser_HPP
+
+#include <string>
+#include <vector>
+#include <cstring>
+//#include "TileSet.hpp"
+#include "Level.hpp"
+#include "Tile.hpp"
+#include "RapidXMLAdapter.hpp"
+
+namespace GameLogic {
+
+    using std::vector;
+    using std::string;
+
+    class WorldParser {
+    public:
+        WorldParser();
+        ~WorldParser();
+
+        Level* generate_level( std::string file_location );
+    };
+
+};
+#endif // !WorldParser_HPP
