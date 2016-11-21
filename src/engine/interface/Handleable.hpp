9c16de4a92c4e975f3dfd3be2b5a0d44526d5bcc
jorgdebont <jacbont@student.avans.nl>
2016-11-21 14:47:09 +0100

    feat: added handleable

---
+++ src/engine/interface/Handleable.hpp
@@ -0,0 +1,16 @@
+//
+// Created by jazula on 11/21/16.
+//
+
+#ifndef MINOR_VIDYA_HANDABLE_HPP
+#define MINOR_VIDYA_HANDABLE_HPP
+
+
+#include "../KeyStrokes.hpp"
+namespace Engine {
+    class Handleable {
+        virtual void handle_input(::Engine::PressedKeys keys) = 0;
+    };
+}
+
+#endif //MINOR_VIDYA_HANDABLE_HPP
