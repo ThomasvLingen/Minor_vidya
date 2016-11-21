ed3095083b58b4a24f82629133991d9f73c924c2
jorgdebont <jacbont@student.avans.nl>
2016-11-21 15:52:24 +0100

    feat: started on interface collection

--- src/engine/interface/Collection.cpp
+++ src/engine/interface/Collection.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Collection.hpp"
+
+namespace Engine {
+
+    void Collection::draw()
+    {
+        for (auto& drawable : this->_drawables) {
+            drawable.draw();
+        }
+    }
+
+    void Collection::handle_input(::Engine::PressedKeys& keys)
+    {
+        for (auto& handleable : this->_handleables) {
+            handleable.handle_input(keys);
+        }
+    }
+
+    void Collection::update(int delta_time)
+    {
+        for (auto& updatable : this->_updatables) {
+            updatable.update(delta_time);
+        }
+    }
+
+    void Collection::add_drawable(Engine::Drawable& to_add)
+    {
+        this->_drawables.push_back(to_add);
+    }
+
+    void Collection::add_handleable(Engine::Handleable& to_add)
+    {
+        this->_handleables.push_back(to_add);
+    }
+
+    void Collection::add_updatable(Engine::Updatable& to_add)
+    {
+        this->_updatables.push_back(to_add);
+    }
+}
--- src/engine/interface/Collection.hpp
+++ src/engine/interface/Collection.hpp
@@ -5,10 +5,30 @@
 #ifndef MINOR_VIDYA_COLLECTION_HPP
 #define MINOR_VIDYA_COLLECTION_HPP
 
+#include "Drawable.hpp"
+#include "Handleable.hpp"
+#include "Updatable.hpp"
+#include <vector>
 
-class Collection {
+namespace Engine {
+    using std::vector;
 
-};
+    class Collection {
+    public:
+        void draw();
+        void handle_input(::Engine::PressedKeys& keys);
+        void update(int delta_time);
+
+        void add_drawable(Drawable& to_add);
+        void add_handleable(Handleable& to_add);
+        void add_updatable(Updatable& to_add);
+
+    private:
+        vector<Drawable> _drawables;
+        vector<Handleable> _handleables;
+        vector<Updatable> _updatables;
+    };
+}
 
 
 #endif //MINOR_VIDYA_COLLECTION_HPP
