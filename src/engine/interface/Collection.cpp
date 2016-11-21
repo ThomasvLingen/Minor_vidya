012a6079ac113c47d7e45c3fe79b501226c0a61a
jorgdebont <jacbont@student.avans.nl>
2016-11-21 14:47:28 +0100

    feat: started on collection class

---
+++ src/engine/interface/Collection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jazula on 11/21/16.
+//
+
+#include "Collection.hpp"
---
+++ src/engine/interface/Collection.hpp
@@ -0,0 +1,14 @@
+//
+// Created by jazula on 11/21/16.
+//
+
+#ifndef MINOR_VIDYA_COLLECTION_HPP
+#define MINOR_VIDYA_COLLECTION_HPP
+
+
+class Collection {
+
+};
+
+
+#endif //MINOR_VIDYA_COLLECTION_HPP
