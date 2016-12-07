f50911ddb3be6736fc5bd4d6268894cf75b3597b
Jelmer <jschoot2@student.avans.nl>
2016-12-07 13:27:13 +0100

    feat: added LevelWinState

---
+++ src/gamelogic/state/LevelWinState.hpp
@@ -0,0 +1,43 @@
+//
+// Created by waseland on 12/7/16.
+//
+
+#ifndef MINOR_VIDYA_LEVELWINSTATE_HPP
+#define MINOR_VIDYA_LEVELWINSTATE_HPP
+
+#include "IGameState.hpp"
+#include "../Game.hpp"
+#include "../../engine/SDLFacade.hpp"
+#include "Menu.hpp"
+
+class LevelWinState {
+    namespace GameLogic {
+            class Game;
+    }
+
+namespace State {
+    using Engine::SDLFacade;
+    using Engine::CoordinateDouble;
+    using Engine::Color;
+    using Engine::FontType;
+    using Engine::PressedKeys;
+    using Engine::Key;
+    using GameLogic::Game;
+
+    class LevelWinState : public IGameState {
+
+    public:
+        LevelWinState(Game& context);
+
+        void update(int time_since_last_update) override;
+
+    private:
+        Menu _menu;
+        int _timeSinceLastPress = 0;
+        Color _color{255,255,255};
+    };
+}
+};
+
+
+#endif //MINOR_VIDYA_LEVELWINSTATE_HPP
