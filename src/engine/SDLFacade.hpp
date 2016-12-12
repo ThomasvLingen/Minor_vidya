4646fac1df093b0ca570b6894179d4be19e38fd1
MFrielink <frielink.m@gmail.com>
2016-12-12 12:57:45 +0100

    refactor: Changed Key enum to new implementation of mapping

--- src/engine/SDLFacade.hpp
+++ src/engine/SDLFacade.hpp
@@ -43,7 +43,7 @@ namespace Engine {
         map<FontType, TTF_Font*> _fonts;
         
         map<string, SDL_Texture*> _images;
-
+        
         map<Key, SDL_Keycode > _possible_keys = {
             {Key::A,            SDLK_a},
             {Key::B,            SDLK_b},
