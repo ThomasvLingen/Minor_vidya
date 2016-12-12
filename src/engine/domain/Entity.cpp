c48880f4e0b8ccea1ab4290ecd0e7eb123cce097
= <=>
2016-12-12 17:35:45 +0100

    docs: added documentation for entity

--- src/engine/domain/Entity.cpp
+++ src/engine/domain/Entity.cpp
@@ -6,6 +6,9 @@
 
 namespace Engine{
 
+    /// \brief Constructor of the class
+    ///
+    /// \param image_buffer The texture of the entity which will be used to draw the entity
     Entity::Entity(ImageBuffer* image_buffer, CoordinateInt* position)
     : _position(position)
     , _image_buffer(image_buffer)
@@ -13,17 +16,22 @@ namespace Engine{
 
     }
 
+    /// \brief Destructor of the class
     Entity::~Entity()
     {
         delete _position;
         delete _image_buffer;
     }
 
+    /// \brief Getter for the position of the entity
+    /// \return returns a CoordinateInt position
     CoordinateInt* Entity::get_position()
     {
         return _position;
     }
 
+    /// \brief Getter for the image buffer of the entity
+    /// \return returns an ImageBuffer
     ImageBuffer* Entity::get_image_buffer()
     {
         return _image_buffer;
