3441eeb684be40e453980902b594a540ac7b1262
SandervGeel <sgeel@avans.nl>
2016-12-13 12:11:38 +0100

    feat: startup of HUD

---
+++ src/gamelogic/HUD.hpp
@@ -0,0 +1,50 @@
+//
+// Created by sander on 12/12/16.
+//
+
+#ifndef MINOR_VIDYA_HUD_HPP
+#define MINOR_VIDYA_HUD_HPP
+
+#include "../engine/interface/Drawable.hpp"
+#include "../engine/interface/Updatable.hpp"
+#include "../engine/SDLFacade.hpp"
+#include "Player.hpp"
+#include <string>
+
+namespace Engine {
+    class SDLFacade;
+}
+
+namespace GameLogic {
+    using std::string;
+    using Engine::Drawable;
+    using Engine::Updatable;
+    using Engine::Color;
+    using Engine::SDLFacade;
+    using Engine::FontType ;
+
+    class HUD : public Drawable, public Updatable {
+    public:
+        HUD(SDLFacade& _SDL_facade, Player* player);
+        HUD(SDLFacade& _SDL_facade);
+
+        void set_ticks_set_off(int ticks);
+        virtual void draw() override;
+
+        virtual void update(int delta_time) override;
+
+    private:
+        Player* _player;
+
+        int _health_points;
+        int _current_time; // in sec
+        int _start_time;
+
+        int _calculate_time(int ticks); // mili seconds to seconds
+        string _time_to_string(int current_time); // nice output 1:32
+
+
+    };
+}
+
+#endif //MINOR_VIDYA_HUD_HPP
