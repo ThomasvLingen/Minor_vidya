340f8cc07b9712d9592a71156664e22d4961d542
joostvanrijsinge <jjrijsin@student.avans.nl>
2016-12-14 20:20:31 +0100

    fix: little fixes

--- src/gamelogic/WorldParser.cpp
+++ src/gamelogic/WorldParser.cpp
@@ -117,7 +117,7 @@ namespace GameLogic {
     /// \param x x position of the tile
     void WorldParser::_set_door_trigger( Level & level, int y, int x )
     {
-        level.get_field[y][x]->add_action_tiletrigger( new TileTrigger( {
+        level.get_field()[y][x]->add_action_tiletrigger( new TileTrigger( {
             [y, x]( Level& level ) {
             level.get_tile_in_level( { y, x } )->set_wall( !level.get_tile_in_level( { y, x } )->is_wall() );
         }}));
@@ -132,8 +132,8 @@ namespace GameLogic {
     /// \param x x position of the tile
     void WorldParser::_set_win_trigger( Level & level, int y, int x )
     {
-        level.get_field[y][x]->add_action_tiletrigger( new TileTrigger( {
-            [y, x]( Level& level ) {
+        level.get_field()[y][x]->add_step_on_tiletrigger( new TileTrigger( {
+            []( Level& level ) {
             level.set_level_over();
         }}));
     }
