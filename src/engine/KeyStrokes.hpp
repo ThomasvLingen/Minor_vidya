dfd8f0de709ced47c86712d8c5f6671acd9edf91
= <=>
2016-12-14 19:07:27 +0100

    fix: removed action enums from engine and replaced them with key enums

--- src/engine/KeyStrokes.hpp
+++ src/engine/KeyStrokes.hpp
@@ -9,9 +9,11 @@
 
 namespace Engine {
     using std::vector;
+    using std::string;
 
-    enum class Key {MOVE_FORWARD, MOVE_BACKWARDS, MOVE_RIGHT, MOVE_LEFT, ROTATE_LEFT, ROTATE_RIGHT, MENU_ENTER, MENU_UP,
-            MENU_DOWN, PAUSE_GAME, ACTION_INTERACT};
+    enum class Key {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+        ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT, SPACE, ESC, ENTER,
+        NUMBER_1, NUMBER_2, NUMBER_3, NUMBER_4, NUMBER_5, NUMBER_6, NUMBER_7, NUMBER_8, NUMBER_9, NUMBER_0};
 
     typedef vector<Key> Keys;
 
