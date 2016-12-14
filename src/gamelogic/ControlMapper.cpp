9f6aa336265f12602c876389e231a4d5d826bdf0
= <=>
2016-12-14 20:48:01 +0100

    fix: moved descriptions to the controlMapper

--- src/gamelogic/ControlMapper.cpp
+++ src/gamelogic/ControlMapper.cpp
@@ -10,6 +10,8 @@ namespace GameLogic{
     : _input_actions(new InputActions())
     {
         this->_init_mapping_info();
+        this->_init_key_descriptions();
+        this->_init_action_descriptions();
     }
 
     ControlMapper::~ControlMapper()
@@ -67,5 +69,84 @@ namespace GameLogic{
     void ControlMapper::set_new_combination(Action action, Key key)
     {
         this->_mapping_info[action] = key;
+    }
+
+    string ControlMapper::get_action_description(Action action)
+    {
+        return this->_action_descriptions[action];
+    }
+
+    string ControlMapper::get_key_description(Key key)
+    {
+        return this->_key_descriptions[key];
+    }
+
+    void ControlMapper::_init_action_descriptions()
+    {
+        this->_action_descriptions[Action::ACTION_INTERACT] = "Interact";
+        this->_action_descriptions[Action::MOVE_BACKWARD] = "Move backward";
+        this->_action_descriptions[Action::MOVE_FORWARD] = "Move forward";
+        this->_action_descriptions[Action::MOVE_LEFT] = "Move left";
+        this->_action_descriptions[Action::MOVE_RIGHT] = "Move right";
+        this->_action_descriptions[Action::ROTATE_LEFT] = "Rotate left";
+        this->_action_descriptions[Action::ROTATE_RIGHT] = "Rotate right";
+        this->_action_descriptions[Action::PAUSE_GAME] = "Pause game";
+    }
+
+    void ControlMapper::_init_key_descriptions()
+    {
+        this->_key_descriptions[Key::A] = "A";
+        this->_key_descriptions[Key::B] = "B";
+        this->_key_descriptions[Key::C] = "C";
+        this->_key_descriptions[Key::D] = "D";
+        this->_key_descriptions[Key::E] = "E";
+        this->_key_descriptions[Key::F] = "F";
+        this->_key_descriptions[Key::G] = "G";
+        this->_key_descriptions[Key::H] = "H";
+        this->_key_descriptions[Key::I] = "I";
+        this->_key_descriptions[Key::J] = "J";
+        this->_key_descriptions[Key::K] = "K";
+        this->_key_descriptions[Key::L] = "L";
+        this->_key_descriptions[Key::M] = "M";
+        this->_key_descriptions[Key::N] = "N";
+        this->_key_descriptions[Key::O] = "O";
+        this->_key_descriptions[Key::P] = "P";
+        this->_key_descriptions[Key::Q] = "Q";
+        this->_key_descriptions[Key::R] = "R";
+        this->_key_descriptions[Key::S] = "S";
+        this->_key_descriptions[Key::T] = "T";
+        this->_key_descriptions[Key::U] = "U";
+        this->_key_descriptions[Key::V] = "V";
+        this->_key_descriptions[Key::W] = "W";
+        this->_key_descriptions[Key::X] = "X";
+        this->_key_descriptions[Key::Y] = "Y";
+        this->_key_descriptions[Key::Z] = "Z";
+        this->_key_descriptions[Key::NUMBER_1] = "1";
+        this->_key_descriptions[Key::NUMBER_2] = "2";
+        this->_key_descriptions[Key::NUMBER_3] = "3";
+        this->_key_descriptions[Key::NUMBER_4] = "4";
+        this->_key_descriptions[Key::NUMBER_5] = "5";
+        this->_key_descriptions[Key::NUMBER_6] = "6";
+        this->_key_descriptions[Key::NUMBER_7] = "7";
+        this->_key_descriptions[Key::NUMBER_8] = "8";
+        this->_key_descriptions[Key::NUMBER_9] = "9";
+        this->_key_descriptions[Key::NUMBER_0] = "0";
+        this->_key_descriptions[Key::ESC] = "Esc";
+        this->_key_descriptions[Key::SPACE] = "Space";
+        this->_key_descriptions[Key::ENTER] = "Enter";
+        this->_key_descriptions[Key::ARROW_UP] = "Arrow up";
+        this->_key_descriptions[Key::ARROW_DOWN] = "Arrow down";
+        this->_key_descriptions[Key::ARROW_LEFT] = "Arrow left";
+        this->_key_descriptions[Key::ARROW_RIGHT] = "Arrow right";
+    }
+
+    map<Key, string>& ControlMapper::get_key_descriptions()
+    {
+        return _key_descriptions;
+    }
+
+    map<Action, string>& ControlMapper::get_action_descriptions()
+    {
+        return _action_descriptions;
     }
 }
--- src/gamelogic/ControlMapper.hpp
+++ src/gamelogic/ControlMapper.hpp
@@ -16,6 +16,7 @@ namespace GameLogic {
     using std::map;
     using Engine::Key;
     using Engine::Input;
+    using std::string;
 
     enum class Action {
         MOVE_FORWARD, MOVE_BACKWARD, MOVE_LEFT, MOVE_RIGHT, ROTATE_LEFT, ROTATE_RIGHT, ACTION_INTERACT, ACTION_USE, MENU_UP, MENU_DOWN, MENU_SELECT, MENU_BACK, PAUSE_GAME
@@ -41,11 +42,21 @@ namespace GameLogic {
 
         Key get_key_by_action(Action action);
 
+        string get_key_description(Key key);
+        string get_action_description(Action action);
+
+        map<Key, string>& get_key_descriptions();
+        map<Action, string>& get_action_descriptions();
+
     private:
         map<Action, Key> _mapping_info;
         InputActions* _input_actions;
+        map<Key, string> _key_descriptions;
+        map<Action, string> _action_descriptions;
 
         void _init_mapping_info();
+        void _init_key_descriptions();
+        void _init_action_descriptions();
     };
 }
 
