c45172ed32ffe0298dacdc11509797f5386bfc3f
= <=>
2016-12-15 00:42:13 +0100

    fix: changed init style for ControlMapper lists

--- src/gamelogic/ControlMapper.cpp
+++ src/gamelogic/ControlMapper.cpp
@@ -44,19 +44,20 @@ namespace GameLogic{
 
     void ControlMapper::_init_mapping_info()
     {
-        this->_mapping_info[Action::MENU_UP] = Key::ARROW_UP;
-        this->_mapping_info[Action::MENU_DOWN] = Key::ARROW_DOWN;
-        this->_mapping_info[Action::MENU_BACK] = Key::ESC;
-        this->_mapping_info[Action::MENU_SELECT] = Key::ENTER;
-        this->_mapping_info[Action::ACTION_INTERACT] = Key::E;
-        //this->_mapping_info[Action::ACTION_USE] = Key::SPACE;
-        this->_mapping_info[Action::MOVE_BACKWARD] = Key::ARROW_DOWN;
-        this->_mapping_info[Action::MOVE_FORWARD] = Key::ARROW_UP;
-        this->_mapping_info[Action::MOVE_LEFT] = Key::A;
-        this->_mapping_info[Action::MOVE_RIGHT] = Key::D;
-        this->_mapping_info[Action::ROTATE_LEFT] = Key::ARROW_LEFT;
-        this->_mapping_info[Action::ROTATE_RIGHT] = Key::ARROW_RIGHT;
-        this->_mapping_info[Action::PAUSE_GAME] = Key::ESC;
+        this->_mapping_info = {
+            {Action::MENU_UP, Key::ARROW_UP},
+            {Action::MENU_DOWN, Key::ARROW_DOWN},
+            {Action::MENU_BACK, Key::ESC},
+            {Action::MENU_SELECT,Key::ENTER},
+            {Action::ACTION_INTERACT, Key::E},
+            {Action::MOVE_BACKWARD, Key::ARROW_DOWN},
+            {Action::MOVE_FORWARD, Key::ARROW_UP},
+            {Action::MOVE_LEFT, Key::A},
+            {Action::MOVE_RIGHT, Key::D},
+            {Action::ROTATE_LEFT, Key::ARROW_LEFT},
+            {Action::ROTATE_RIGHT, Key::ARROW_RIGHT},
+            {Action::PAUSE_GAME, Key::ESC}
+        };
     }
 
     Key ControlMapper::get_key_by_action(Action action)
@@ -81,61 +82,65 @@ namespace GameLogic{
 
     void ControlMapper::_init_action_descriptions()
     {
-        this->_action_descriptions[Action::ACTION_INTERACT] = "Interact";
-        this->_action_descriptions[Action::MOVE_BACKWARD] = "Move backward";
-        this->_action_descriptions[Action::MOVE_FORWARD] = "Move forward";
-        this->_action_descriptions[Action::MOVE_LEFT] = "Move left";
-        this->_action_descriptions[Action::MOVE_RIGHT] = "Move right";
-        this->_action_descriptions[Action::ROTATE_LEFT] = "Rotate left";
-        this->_action_descriptions[Action::ROTATE_RIGHT] = "Rotate right";
-        this->_action_descriptions[Action::PAUSE_GAME] = "Pause game";
+        this->_action_descriptions = {
+            {Action::ACTION_INTERACT, "Interact"},
+            {Action::MOVE_BACKWARD, "Move backward"},
+            {Action::MOVE_FORWARD, "Move forward"},
+            {Action::MOVE_LEFT, "Move left"},
+            {Action::MOVE_RIGHT, "Move right"},
+            {Action::ROTATE_LEFT, "Rotate left"},
+            {Action::ROTATE_RIGHT, "Rotate right"},
+            {Action::PAUSE_GAME, "Pause game"}
+        };
     }
 
     void ControlMapper::_init_key_descriptions()
     {
-        this->_key_descriptions[Key::A] = "A";
-        this->_key_descriptions[Key::B] = "B";
-        this->_key_descriptions[Key::C] = "C";
-        this->_key_descriptions[Key::D] = "D";
-        this->_key_descriptions[Key::E] = "E";
-        this->_key_descriptions[Key::F] = "F";
-        this->_key_descriptions[Key::G] = "G";
-        this->_key_descriptions[Key::H] = "H";
-        this->_key_descriptions[Key::I] = "I";
-        this->_key_descriptions[Key::J] = "J";
-        this->_key_descriptions[Key::K] = "K";
-        this->_key_descriptions[Key::L] = "L";
-        this->_key_descriptions[Key::M] = "M";
-        this->_key_descriptions[Key::N] = "N";
-        this->_key_descriptions[Key::O] = "O";
-        this->_key_descriptions[Key::P] = "P";
-        this->_key_descriptions[Key::Q] = "Q";
-        this->_key_descriptions[Key::R] = "R";
-        this->_key_descriptions[Key::S] = "S";
-        this->_key_descriptions[Key::T] = "T";
-        this->_key_descriptions[Key::U] = "U";
-        this->_key_descriptions[Key::V] = "V";
-        this->_key_descriptions[Key::W] = "W";
-        this->_key_descriptions[Key::X] = "X";
-        this->_key_descriptions[Key::Y] = "Y";
-        this->_key_descriptions[Key::Z] = "Z";
-        this->_key_descriptions[Key::NUMBER_1] = "1";
-        this->_key_descriptions[Key::NUMBER_2] = "2";
-        this->_key_descriptions[Key::NUMBER_3] = "3";
-        this->_key_descriptions[Key::NUMBER_4] = "4";
-        this->_key_descriptions[Key::NUMBER_5] = "5";
-        this->_key_descriptions[Key::NUMBER_6] = "6";
-        this->_key_descriptions[Key::NUMBER_7] = "7";
-        this->_key_descriptions[Key::NUMBER_8] = "8";
-        this->_key_descriptions[Key::NUMBER_9] = "9";
-        this->_key_descriptions[Key::NUMBER_0] = "0";
-        this->_key_descriptions[Key::ESC] = "Esc";
-        this->_key_descriptions[Key::SPACE] = "Space";
-        this->_key_descriptions[Key::ENTER] = "Enter";
-        this->_key_descriptions[Key::ARROW_UP] = "Arrow up";
-        this->_key_descriptions[Key::ARROW_DOWN] = "Arrow down";
-        this->_key_descriptions[Key::ARROW_LEFT] = "Arrow left";
-        this->_key_descriptions[Key::ARROW_RIGHT] = "Arrow right";
+        this->_key_descriptions = {
+            {Key::A, "A"},
+            {Key::B, "B"},
+            {Key::C, "C"},
+            {Key::D, "D"},
+            {Key::E, "E"},
+            {Key::F, "F"},
+            {Key::G, "G"},
+            {Key::H, "H"},
+            {Key::I, "I"},
+            {Key::J, "J"},
+            {Key::K, "K"},
+            {Key::L, "L"},
+            {Key::M, "M"},
+            {Key::N, "N"},
+            {Key::O, "O"},
+            {Key::P, "P"},
+            {Key::Q, "Q"},
+            {Key::R, "R"},
+            {Key::S, "S"},
+            {Key::T, "T"},
+            {Key::U, "U"},
+            {Key::V, "V"},
+            {Key::W, "W"},
+            {Key::X, "X"},
+            {Key::Y, "Y"},
+            {Key::Z, "Z"},
+            {Key::NUMBER_1, "1"},
+            {Key::NUMBER_2, "2"},
+            {Key::NUMBER_3, "3"},
+            {Key::NUMBER_4, "4"},
+            {Key::NUMBER_5, "5"},
+            {Key::NUMBER_6, "6"},
+            {Key::NUMBER_7, "7"},
+            {Key::NUMBER_8, "8"},
+            {Key::NUMBER_9, "9"},
+            {Key::NUMBER_0, "0"},
+            {Key::ESC, "Esc"},
+            {Key::SPACE, "Space"},
+            {Key::ENTER, "Enter"},
+            {Key::ARROW_UP, "Arrow up"},
+            {Key::ARROW_DOWN, "Arrow down"},
+            {Key::ARROW_LEFT, "Arrow left"},
+            {Key::ARROW_RIGHT, "Arrow right"}
+        };
     }
 
     map<Key, string>& ControlMapper::get_key_descriptions()
